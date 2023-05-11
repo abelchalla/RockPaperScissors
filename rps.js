169629039e93afd48674446ce25bde772fa4b9dd
Abel Challa <abelchalla22@gmail.com>
2023-05-10 17:21:51 -0700

    Add a function that plays one round of RPS

--- rps.js
+++ rps.js
@@ -10,3 +10,35 @@ function getComputerChoice() {
         return 'Scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toLowerCase() == 'rock' && computerSelection == 'Paper') {
+        return "You Lose! Paper beats Rock";
+    }
+    else if(playerSelection.toLowerCase() == 'paper' && computerSelection == 'Paper') {
+        return "It's a Tie!";
+    }
+    else if(playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Paper') {
+        return "You Win! Scissors beats Paper";
+    }
+
+    else if(playerSelection.toLowerCase() == 'rock' && computerSelection == 'Rock') {
+        return "It's a Tie!";
+    }
+    else if(playerSelection.toLowerCase() == 'paper' && computerSelection == 'Rock') {
+        return "You Win! Paper beats Rock";
+    }
+    else if(playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Rock') {
+        return "You Lose! Rock beats Scissors";
+    }
+
+    else if(playerSelection.toLowerCase() == 'rock' && computerSelection == 'Scissors') {
+        return "You Win! Rock beats Scissors";
+    }
+    else if(playerSelection.toLowerCase() == 'paper' && computerSelection == 'Scissors') {
+        return "You Lose! Scissors beats Paper";
+    }
+    else if(playerSelection.toLowerCase() == 'scissors' && computerSelection == 'Scissors') {
+        return "It's a Tie!";
+    }
+}
